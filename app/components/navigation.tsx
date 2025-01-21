5c069e31db48cd7066861790fc60965be59a5f70
Joe Aguado <aguado.joe@gmail.com>
2025-01-21 11:50:01 -0600

    navigation updated

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -11,7 +11,7 @@ const navigation = () => {
   <header className="absolute inset-x-0 top-0 z-50">
     <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div className="flex lg:flex-1">
-        <a href="/" className="-m-1.5 p-1.5">
+        <a href="/remix-spa-demo" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
           <img className="h-8 w-auto" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" alt=""/>
         </a>
